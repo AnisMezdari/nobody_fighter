0b10f2596e555baf6fa003258309c956bd5e4cef
Antoine Drouhin <antoine.drouhin@gmail.com>
2017-12-08 06:08:58 +0100

    Fight

--- web/index.js
+++ web/index.js
@@ -1,39 +1,93 @@
 
-   var img1 = new Image();
-   img1.crossOrigin="Anonymous";
-   img1.src = "./img/Sprites-y-mia/y-mia-normal(b).gif";
-   var img2 = new Image();
-   img2.crossOrigin="Anonymous";
-   img2.src = "./img/Sprites-y-mia/y-mia-objection.gif";
 
 var stage = new createjs.Stage('nobody_fight');
-var ss = new createjs.SpriteSheet({
+
+// MIA
+var mia = new createjs.SpriteSheet({
     frames: {
         width: 256,
         height: 192,
         numFrames: 3
     },
     animations: {stand: [0], ouade1: [0,1, "ouade2"],ouade2: [1,2, "stand"]},
-    images: ["./img/mia/y-mia-normal(b).gif","./img/mia/y-mia-objection(1).gif","./img/mia/y-mia-objection(2).gif"]
+    images: [
+        "./img/mia/y-mia-normal(b).gif",
+        "./img/mia/y-mia-objection(1).gif",
+        "./img/mia/y-mia-objection(2).gif"]
 });
     
-var sprite = new createjs.Sprite(ss, "stand");
-sprite.scaleY = sprite.scaleX = 1.5;
-stage.addChild(sprite);
+var spriteMia = new createjs.Sprite(mia, "stand");
+spriteMia.scaleY = spriteMia.scaleX = 1.5;
+stage.addChild(spriteMia);
    
 createjs.Ticker.on("tick", stage);
+
+$(window).keyup(
+    function(e){
+        switch(e.keyCode ) {
+            case 83:
+                spriteMia.gotoAndPlay("ouade1");
+                break;
+        }
+})
 
 $(window).keydown(
     function(e){
         switch(e.keyCode ) {
-            case 13:
-                sprite.gotoAndPlay("ouade1");
+            case 81:
+                spriteMia.x-=20;
                 break;
+            case 68:
+                spriteMia.x+=20;
+                break;
+        }
+})
+
+// GODOT
+var godot = new createjs.SpriteSheet({
+    frames: {
+        width: 256,
+        height: 192,
+        numFrames: 3
+    },
+    animations: {
+        stand: [0],
+        strike1: [0,1, "strike2"],
+        strike2: [1,2, "strike3"],
+        strike3: [2,3, "stand"]
+    },
+    images: [
+        "./img/godot/godot-rest(1).gif",
+        "./img/godot/godot-strike(1).gif",
+        "./img/godot/godot-strike(2).gif",
+        "./img/godot/godot-strike(3).gif"
+    ]
+})
+    
+var spriteGodot = new createjs.Sprite(godot, "stand");
+spriteGodot.scaleY = spriteGodot.scaleX = 1.5;
+spriteGodot.x = 800;
+stage.addChild(spriteGodot);
+   
+createjs.Ticker.on("tick", stage);
+
+$(window).keyup(
+    function(e){
+        switch(e.keyCode ) {
+            case 13:
+                spriteGodot.gotoAndPlay("strike1");
+                break;
+        }
+})
+
+$(window).keydown(
+    function(e){
+        switch(e.keyCode ) {
             case 37:
-                sprite.x-=20;
+                spriteGodot.x-=20;
                 break;
             case 39:
-                sprite.x+=20;
+                spriteGodot.x+=20;
                 break;
         }
 })
