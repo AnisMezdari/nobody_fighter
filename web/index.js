debc93f4294ccd1bab41b1513232f2591c2ee7c7
Anis Mezdari <mezdari77580@gmail.com>
2017-12-13 22:14:53 +0100

    ajout one puch man perso

--- web/index.js
+++ web/index.js
@@ -1,93 +1,183 @@
+var canvas = document.getElementById("nobody_fight");
+var ctx = canvas.getContext("2d");
+canvas.width = 1200;
+canvas.height = 680;
+
+document.body.appendChild(canvas);
+
+var heroImage = new Image();
+heroImage.src = 'img/onepuchMan/1-persoGaucheInitiale.png';
+
+var heroImage2 = new Image();
+heroImage2.src = 'img/onepuchMan/7-persoDroiteInitiale.png';
+
+var hero = {
+	speed: 400, // movement in pixels per second
+	x: 0,
+	y: 0
+};
+
+var hero2 = {
+	speed: 400, // movement in pixels per second
+	x: 800,
+	y: 330
+};
+var keysDown = {};
+
+var estAttcker = false;
+
+  addEventListener("keydown", function (e) {
+
+    if(40 == e.keyCode){
+        estAttcker = true;
+    }
+    keysDown[e.keyCode] = true;
+
+  }, false);
+
+addEventListener("keyup", function (e) {
+	delete keysDown[e.keyCode];
+}, false);
 
 
-var stage = new createjs.Stage('nobody_fight');
+var reset = function () {
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
+};
 
-// MIA
-var mia = new createjs.SpriteSheet({
-    frames: {
-        width: 256,
-        height: 192,
-        numFrames: 3
-    },
-    animations: {stand: [0], ouade1: [0,1, "ouade2"],ouade2: [1,2, "stand"]},
-    images: [
-        "./img/mia/y-mia-normal(b).gif",
-        "./img/mia/y-mia-objection(1).gif",
-        "./img/mia/y-mia-objection(2).gif"]
-});
-    
-var spriteMia = new createjs.Sprite(mia, "stand");
-spriteMia.scaleY = spriteMia.scaleX = 1.5;
-stage.addChild(spriteMia);
-   
-createjs.Ticker.on("tick", stage);
 
-$(window).keyup(
-    function(e){
-        switch(e.keyCode ) {
-            case 83:
-                spriteMia.gotoAndPlay("ouade1");
-                break;
-        }
-})
 
-$(window).keydown(
-    function(e){
-        switch(e.keyCode ) {
-            case 81:
-                spriteMia.x-=20;
-                break;
-            case 68:
-                spriteMia.x+=20;
-                break;
-        }
-})
+var i = 0;
+// Update game objects
+var update = function (modifier) {
+	if (38 in keysDown) { // Player holding up
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+		// hero.y -= hero.speed * modifier;
+	}
+	if (40 in keysDown) { // Player holding down
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+		// hero.y += hero.speed * modifier;
+    // var now  = Date.now();
+      attack();
+      i++;
 
-// GODOT
-var godot = new createjs.SpriteSheet({
-    frames: {
-        width: 256,
-        height: 192,
-        numFrames: 3
-    },
-    animations: {
-        stand: [0],
-        strike1: [0,1, "strike2"],
-        strike2: [1,2, "strike3"],
-        strike3: [2,3, "stand"]
-    },
-    images: [
-        "./img/godot/godot-rest(1).gif",
-        "./img/godot/godot-strike(1).gif",
-        "./img/godot/godot-strike(2).gif",
-        "./img/godot/godot-strike(3).gif"
-    ]
-})
-    
-var spriteGodot = new createjs.Sprite(godot, "stand");
-spriteGodot.scaleY = spriteGodot.scaleX = 1.5;
-spriteGodot.x = 800;
-stage.addChild(spriteGodot);
-   
-createjs.Ticker.on("tick", stage);
+    estAttcker = false;
+	}
+	if (37 in keysDown) { // Player holding left
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    heroImage.src = 'img/onepuchMan/6-persoGaucheRecule1.png';
+		hero.x -= hero.speed * modifier;
+	}
+	if (39 in keysDown) { // Player holding right
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    heroImage.src = 'img/onepuchMan/1-persoGaucheInitiale.png';
+    animationAvance();
+		hero.x += hero.speed * modifier;
+	}
+  if(81 in keysDown) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    hero2.x -= hero2.speed * modifier;
+  }
+  if(68 in keysDown) {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    hero2.x += hero2.speed * modifier;
+  }
 
-$(window).keyup(
-    function(e){
-        switch(e.keyCode ) {
-            case 13:
-                spriteGodot.gotoAndPlay("strike1");
-                break;
-        }
-})
+};
 
-$(window).keydown(
-    function(e){
-        switch(e.keyCode ) {
-            case 37:
-                spriteGodot.x-=20;
-                break;
-            case 39:
-                spriteGodot.x+=20;
-                break;
-        }
-})
+
+var render = function () {
+		ctx.drawImage(heroImage, hero.x, hero.y);
+    ctx.drawImage(heroImage2, hero2.x, hero2.y);
+};
+
+
+var main = function () {
+	var now = Date.now();
+	var delta = now - then;
+
+	update(delta / 1000);
+	render();
+
+	then = now;
+
+	// Request to do this again ASAP
+	requestAnimationFrame(main);
+};
+
+
+var then = Date.now();
+reset();
+main();
+
+
+function animationAvance(){
+
+  heroImage.src = "img/onepuchMan/2-persoGaucheAvance1.png";
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  // perso.style = "width : 120px;";
+  setTimeout(function(){
+    heroImage.src = "img/onepuchMan/3-persoGaucheAvance2.png";
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    // perso.style = "width : 120px;";
+    setTimeout(function(){
+      heroImage.src = "img/onepuchMan/4-persoGaucheAvance3.png";
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      setTimeout(function(){
+        heroImage.src = "img/onepuchMan/5-persoGaucheAvance4.png";
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        setTimeout(function(){
+          heroImage.src = "img/onepuchMan/2-persoGaucheAvance1.png";
+          ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        },100);
+      },100);
+    },70);
+  },70);
+}
+
+
+function attack(){
+    estAttcker = true;
+    // var now = Date.now();
+
+    // if((now - momentDuClick) > 50000 ||  i == 0){
+      heroImage.src = "image/attaqueIop/iop1.png";
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      // perso.style = "width : 120px;";
+      setTimeout(function(){
+        heroImage.src = "image/attaqueIop/iop2.png";
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        // perso.style = "width : 120px;";
+        setTimeout(function(){
+          heroImage.src = "image/attaqueIop/iop3.png";
+          ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+          // perso.style = "width : 120px;";
+          setTimeout(function(){
+            heroImage.src = "image/attaqueIop/iop4.png";
+            ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+            // perso.style = "width : 120px;";
+            setTimeout(function(){
+              heroImage.src = "image/attaqueIop/iop5.png";
+              ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+              // perso.style = "width : 120px;";
+              setTimeout(function(){
+                heroImage.src = "image/attaqueIop/iop6.png";
+                ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+                // perso.style = "width : 120px;";
+                setTimeout(function(){
+                  heroImage.src = "image/attaqueIop/iop7.png";
+                  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+                  setTimeout(function(){
+                    heroImage.src = "image/attaqueIop/iop0.png";
+                    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+                    // perso.style = "width : 120px;";
+                  },200);
+                  // perso.style = "width : 120px;";
+                },85);
+              },80);
+            },80);
+          },70);
+        },60);
+      },50);
+    // }
+  }
